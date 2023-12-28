3b883b28994631e32a48a4b8e93d2c797dc8beba
repmacrypn <artik3267@gmail.com>
2023-12-28 23:26:12 +0300

    fix: created and designed contacts page

---
+++ src/constants/contactsPageData.ts
@@ -0,0 +1,18 @@
+import twitter from '@/assets/images/twitter.svg'
+import instagram from '@/assets/images/instagram.svg'
+import facebook from '@/assets/images/facebook.svg'
+import linkedin from '@/assets/images/linkedin.svg'
+
+export const contacts = [
+  { contact: 'Email', link: 'contact@modsen-software.com', linkType: 'mailto:' },
+  { contact: 'Phone', link: '+48501157180', linkType: 'tel:' },
+  { contact: 'Stefana Okrzei 1a/10' },
+  { contact: 'Warsaw, Poland' },
+]
+
+export const mediaLinks = [
+  { name: 'twitter', link: 'https://www.modsen-software.com', icon: twitter },
+  { name: 'instagram', link: 'https://www.modsen-software.com', icon: instagram },
+  { name: 'facebook', link: 'https://www.modsen-software.com', icon: facebook },
+  { name: 'linkedin', link: 'https://www.modsen-software.com', icon: linkedin },
+]
