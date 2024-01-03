eea01fce663c7d33da92f1ce25fa4ffb1ce17a09
repmacrypn <artik3267@gmail.com>
2024-01-03 22:17:02 +0300

    fix: fixed bug with date control

--- src/utils/helpers/handleDateControl.ts
+++ src/utils/helpers/handleDateControl.ts
@@ -2,12 +2,13 @@ import { periodEnum } from '@/types/timeline'
 
 export const handleDateControl = (period: periodEnum = periodEnum.Day) => {
   const currentDate = new Date()
-  const month =
+
+  let month =
     period === periodEnum.Month
       ? `0${currentDate.getMonth()}`.slice(-2)
       : `0${currentDate.getMonth() + 1}`.slice(-2)
   const day = `0${currentDate.getDate() - 1}`.slice(-2)
-  const year = currentDate.getFullYear()
+  let year = currentDate.getFullYear()
   const daysInMonth = new Date(
     currentDate.getFullYear(),
     currentDate.getMonth() + 1,
@@ -15,6 +16,11 @@ export const handleDateControl = (period: periodEnum = periodEnum.Day) => {
   ).getDate()
   const daysArr = Array.from({ length: daysInMonth }, (_, i) => (i + 1).toString())
   const pastDaysArr = daysArr.filter((day) => parseInt(day, 10) < currentDate.getDate())
+
+  if (period === periodEnum.Month && currentDate.getMonth() === 0) {
+    year -= 1
+    month = '12'
+  }
 
   return { day, month, year, pastDaysArr }
 }
